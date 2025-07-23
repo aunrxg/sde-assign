924c1b78a61cd496f0716000ee6de5ca8f13f139
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-07-23 12:07:35 +0530

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("chal gaya");
+});
+
 console.log("hello");
 console.log("OK");
+
+app.listen(port, () => {
+  return console.log(`Express server running at http://localhost:${port}`);
+});

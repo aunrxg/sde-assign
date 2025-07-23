a6f29a815025cd6082ddf0ab41891921db7e48be
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-07-23 21:18:36 +0530

    add create and update controller

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,11 @@ const app = express();
 app.use(express.json({ limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+//routes
+import taskRouter from "@routes/task.route.js";
+
+//routes declaration
+app.use("/api/v1/tasks", taskRouter);
+
 
 export default app;
--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,25 +1,64 @@
 import { Request, Response } from "express";
 import { 
   createTask,
-  getAllTask,
-  getTaskById,
   updateTask,
-  deleteTask,
  } from "@db/task.quries.js";
-import { createTaskSchema } from "@utils/validation.js";
+import { createTaskSchema, updateTaskSchema } from "../validation/task.schema.js";
+import { asyncHandler } from "@utils/asyncHandler.js";
+import { ApiError } from "@utils/apiError.js";
+import { ApiResponse } from "@utils/apiResponse.js";
 
+export const handleCreateTask = asyncHandler(async (req: Request, res: Response) => {
 
-export const handleCreateTask = async (req: Request, res: Response) => {
-  try {
-    const result = createTaskSchema.safeParse(req.body);
-    if(!result.success) {
-      return res.status(400).json({ error: result.error.flatten().fieldErrors });
-    }
-    const data = result.data;
-    const newTask = await createTask(req.body);
-    res.status(201).json(newTask);
-  } catch (error) {
-    console.error("Could not create Task: ", error);
-    res.status(500).json({ error: "Failed to create task." });
+  // validate body inputs
+  const parsed = createTaskSchema.safeParse(req.body);
+  if(!parsed.success) {
+    const error = Object.entries(parsed.error.flatten().fieldErrors).flatMap(([field, msgs]) => 
+      (msgs || []).map((msg) => `${field}: ${msg}`)
+    );
+
+    throw new ApiError(400, "Validation Error", error);
   }
-}
+
+  try {
+    const newTask = await createTask(parsed.data);
+    if(!newTask) {
+      throw new ApiError(400, "");
+    }
+    return res
+      .status(201)
+      .json(new ApiResponse(201, { task: newTask }, "Task Added succesfully"));
+  } catch (error) {
+    console.error(error);
+    throw new ApiError(500, "Failed to creaet task, something went wrong");
+  }
+
+});
+
+export const handleUpdateTask = asyncHandler(async (req: Request, res: Response) => {
+
+  // validate body inputs
+  const parsed = updateTaskSchema.safeParse(req.body);
+
+  if(!parsed.success) {
+    const error = Object.entries(parsed.error.flatten().fieldErrors).flatMap(([field, msgs]) => 
+      (msgs || []).map((msg) => `${field}: ${msg}`)
+    );
+    throw new ApiError(400, "Validation Error", error);
+  }
+
+  try {
+    const updatedTask = await updateTask(parsed.data.id, parsed.data);
+    if(!updatedTask) {
+      throw new ApiError(404, "Task not found")
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { task: updatedTask }, "Task updated successfully"));
+  } catch (error) {
+    console.error(error);
+    throw new ApiError(500, "Failed to update task");
+  }
+});
+

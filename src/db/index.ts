b8f31a7df03d84248fa29a3924633149c6dadb54
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-07-24 18:16:14 +0530

    add table creation query

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,11 +10,29 @@ export const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
+const initTasksTable = async () => {
+  const createTableSQL = `
+    CREATE TABLE IF NOT EXIST tasks (
+      id VARCHAR(36) PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      decription TEXT,
+      dueDate DATETIME NULL,
+      status ENUM('PENDING', 'COMPLETED', 'IN_PROGRESS') NOT NULL DEFAULT 'PENDING',
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      );
+  `;
+
+  await pool.query(createTableSQL);
+  console.log("Task Table is ready");
+}
+
 const connectDB = async () => {
   try {
     const connection = await pool.getConnection();
     console.log(`\nMySQL Connected!! THREAD ID: ${connection.threadId}`);
     connection.release();
+    await initTasksTable();
     return pool;
   } catch (error) {
     console.error("MySql Connection Error: ", error);
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,9 @@ dotenv.config({
 });
 
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is Running at http://localhost:${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("DB Connection FAiled: ", err);
-  })
+(async () => {
+  await connectDB();
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running at PORT: ${process.env.PORT}`);
+  });
+})();

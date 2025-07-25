2f998ae631f75ebafbbfa84ff24633afe2e1d317
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-07-25 12:05:51 +0530

    fix validation middlware

--- src/middlewares/validationRequest.middleware.ts
+++ src/middlewares/validationRequest.middleware.ts
@@ -1,33 +1,32 @@
 import { ApiError } from "@utils/apiError.js";
 import { NextFunction, Request, Response } from "express";
-import { ZodObject } from "zod/v4";
+import { ZodError, ZodObject } from "zod/v4";
 // import { AnyZodObject } from "zod/v3";
 
+
+// middlware to validate routes
 export const validationRequest =
-  (schema: ZodObject) => (req: Request, _: Response, next: NextFunction) => {
-    try {
-      const parsed = schema.safeParse({
-        body: req.body,
-        params: req.params,
-        query: req.query,
-      });
-
-      if (!parsed.success) {
-        const { fieldErrors } = parsed.error.flatten();
-        const errorMessages = Object.entries(fieldErrors).flatMap(
-          ([field, msgs]) => (msgs || []).map((msg) => `${field}: ${msg}`)
-        );
-
-        return next(new ApiError(400, "Validation Error", errorMessages));
-      }
-
-      // replacing req object with parsed data
-      (req as any).validatedData = parsed.data;
-
-      next();
-    } catch (error) {
-      next(error);
-      // const errorMsg = error instanceof Error ? error.message : "Task Request Failed";
-      // throw new ApiError(401, errorMsg);
+  (schema: ZodObject<any>) => (req: Request, _res: Response, next: NextFunction) => {
+    
+    const parsed = schema.safeParse({
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    });
+    
+    if (!parsed.success) {
+      const formattedError = formatZodErrors(parsed.error);
+      return next(new ApiError(422, "Validation failed", formattedError));
     }
+
+    (req as any).validatedData = parsed.data;
+    next();
   };
+
+
+const formatZodErrors = (error: ZodError) => {
+  const { fieldErrors } = error.flatten();
+  return Object.entries(fieldErrors).flatMap(([field, msgs]) =>
+    (msgs as string[]).map((msg) => ({ field, message: msg }))
+  );
+};

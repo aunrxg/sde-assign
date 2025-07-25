123ee7e6f89696315c399838e932b75a6aebae16
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-07-25 12:05:25 +0530

    fix error msgs and trycatch duplication

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -6,105 +6,71 @@ import {
   getAllTasks,
   getTaskById,
   updateTask,
-} from "@db/task.quries.js";
+} from "@db/tasks.queries.js";
 import { asyncHandler } from "@utils/asyncHandler.js";
 import { ApiError } from "@utils/apiError.js";
 import { ApiResponse } from "@utils/apiResponse.js";
 
+
+// create task controller
 export const handleCreateTask = asyncHandler(
   async (req: Request, res: Response) => {
-    try {
-      const { title, description, dueDate, status } = (req as any).validatedData
-        .body;
-      const data = { title, description, dueDate, status };
-      const newTask = await createTask(data);
-      return res
-        .status(201)
-        .json(
-          new ApiResponse(201, { task: newTask }, "Task added successfully")
-        );
-    } catch (error) {
-      console.error("Create Task failed: ", error);
-      throw new ApiError(500, "Failed to create task");
-    }
+    const { title, description, dueDate, status } = (req as any).validatedData.body;
+    const task = await createTask({ title, description, dueDate, status });
+
+    res 
+      .status(201)
+      .json(new ApiResponse(201, { task }, "Task created successfully"));
   }
 );
 
+
+// get all task controller
 export const handleGetAllTasks = asyncHandler(
-  async (_: Request, res: Response) => {
-    try {
-      const tasks = await getAllTasks();
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(200, { tasks: tasks }, "Tasks fetched Successfully")
-        );
-    } catch (error) {
-      throw new ApiError(500, "Couldn't fetch tasks, try later");
-    }
+  async (_req: Request, res: Response) => {
+    const tasks = await getAllTasks();
+    res.status(200).json(new ApiResponse(200, { tasks }, "Tasks fetched successfully"))
   }
 );
 
+
+// get task by id controller
 export const handleGetTaskById = asyncHandler(
   async (req: Request, res: Response) => {
-    try {
-      const { id } = (req as any).validatedData.params;
-      const task = await getTaskById(id);
-      if (!task) throw new ApiError(404, "No Task with this ID");
-      return res
-        .status(200)
-        .json(new ApiResponse(200, { task }, "Task fetch successfully"));
-    } catch (error) {
-      throw new ApiError(500, "Something went wrong");
-    }
+    const { id } = (req as any).validatedData.params;
+    const task = await getTaskById(id);
+    if(!task) throw new ApiError(404, "Task not found");
+    res.status(200).json(new ApiResponse(200, { task }, "Task fetched successfully"));
   }
 );
 
+
+// update task controller
 export const handleUpdateTask = asyncHandler(
   async (req: Request, res: Response) => {
-    try {
-      const { id } = (req as any).validatedData.params;
-      const updated = (req as any).validatedData.body;
+    const { id } = (req as any).validatedData.params;
+    const updates = (req as any).validatedData.body;
 
-      const task = await getTaskById(id);
-      if (!task) throw new ApiError(404, "task not found");
+    const existing = await getTaskById(id);
+    if(!existing) throw new ApiError(404, "Task not found");
 
-      const updatedTask = await updateTask(id, updated);
-
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(
-            200,
-            { task: updatedTask },
-            "Task updated successfully"
-          )
-        );
-    } catch (error) {
-      console.error("Error: hehehe: ", error);
-      throw new ApiError(500, "Server issue");
-    }
+    const updatedTask = await updateTask(id, updates);
+    res.status(200).json(new ApiResponse(200, { task: updatedTask }, "Task updated successfully"));
   }
 );
 
+
+// delete task controller
 export const handleDeleteTask = asyncHandler(
   async (req: Request, res: Response) => {
-    try {
-      const { id } = (req as any).validatedData.params;
-      
-      const task = await getTaskById(id);
-      if(!task) {
-        throw new ApiError(404, "No Task found");
-      }
-      const deleted = await deleteTask(id);
+    const { id } = (req as any).validatedData.params;
 
-      if (!deleted) throw new ApiError(404, "Task not found");
+    const existing = await getTaskById(id);
+    if(!existing) throw new ApiError(404, "Task not found");
 
-      return res
-        .status(200)
-        .json(new ApiResponse(200, "Task Deleted successfully"));
-    } catch (error) {
-      throw new ApiError(500, "Server issue");
-    }
+    const deleted = await deleteTask(id);
+    if(!deleted) throw new ApiError(500, "Failed to delete task");
+
+    res.status(204).end();
   }
 );
